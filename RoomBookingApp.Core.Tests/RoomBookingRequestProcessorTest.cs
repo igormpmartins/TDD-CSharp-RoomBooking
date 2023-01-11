9afc9acb105ef98732c7ce2be2cf3147c80788d9
Igor Martins-WDG <igor.martins-wdg@ibm.com>
2023-01-11 08:16:58 -0300

    Add project files.

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,38 @@
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessorTest
+    {
+
+        [Fact]
+        public void Should_Return_Room_Booking_Respose_With_Request_Values()
+        {
+            //Arrange
+            var request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "teste@request.com",
+                Date = new DateTime(2021, 10, 20)
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            //Act
+            RoomBookingResult result = processor.BookRoom(request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+            /*result.ShouldNotBeNull();
+            result.FullName.ShouldBe(result.FullName);
+            result.Email.ShouldBe(result.Email);
+            result.Date.ShouldBe(result.Date);*/
+
+        }
+    }
+}
---
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -0,0 +1,20 @@
+﻿using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core.Processors
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        public RoomBookingResult BookRoom(RoomBookingRequest request)
+            => new RoomBookingResult
+            {
+                FullName = request.FullName,
+                Date = request.Date,
+                Email = request.Email
+            };
+
+    }
+}
