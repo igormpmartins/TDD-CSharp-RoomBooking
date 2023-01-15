d6b4bc486244feedd1d0510978f06d0af347dde5
Igor Martins-WDG <igor.martins-wdg@ibm.com>
2023-01-15 13:45:55 -0300

    add integration tests

--- RoomBookingApp.Core/DataServices/IRoomBookingService.cs
+++ RoomBookingApp.Core/DataServices/IRoomBookingService.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using RoomBookingApp.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RoomBookingApp.Core.Domain;
 
 namespace RoomBookingApp.Core.DataServices
 {
---
+++ RoomBookingApp.Persistence/Repositories/RoomBookingService.cs
@@ -0,0 +1,43 @@
+﻿using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBookingApp.Persistence.Repositories
+{
+    public class RoomBookingService : IRoomBookingService
+    {
+        private RoomBookingAppDbContext context;
+
+        public RoomBookingService(RoomBookingAppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Room> GetAvailableRooms(DateTime date)
+        {
+            /*
+            var unavailableRooms = context.RoomBookings
+                .Where(q => q.Date == date)
+                .Select(q => q.RoomId)
+                .ToList();
+
+            var availableRooms = context.Rooms.Where(q => !unavailableRooms.Contains(q.Id)).ToList();*/
+
+            var availableRooms = context.Rooms
+                .Where(q => !q.RoomBookings.Any(q => q.Date  == date))
+                .ToList();
+
+            return availableRooms;
+        }
+
+        public void Save(RoomBooking roomBooking)
+        {
+            context.Add(roomBooking);
+            context.SaveChanges();
+        }
+    }
+}
