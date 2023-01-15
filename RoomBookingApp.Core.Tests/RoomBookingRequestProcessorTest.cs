ab432743f1a9f01f2bc46e6bbb93ca862b6cd826
Igor Martins-WDG <igor.martins-wdg@ibm.com>
2023-01-15 10:50:44 -0300

    add unit testing for application core

--- RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -1,23 +1,45 @@
-﻿using RoomBookingApp.Core.Models;
+﻿using Moq;
+using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Core.Domain;
+using RoomBookingApp.Core.Enums;
+using RoomBookingApp.Core.Models;
 using RoomBookingApp.Core.Processors;
+using Shouldly;
 
 namespace RoomBookingApp.Core
 {
     public class RoomBookingRequestProcessorTest
     {
+        private RoomBookingRequestProcessor processor;
+        private RoomBookingRequest request;
+        private Mock<IRoomBookingService> roomBookingServiceMock;
+        private List<Room> availableRooms;
 
-        [Fact]
-        public void Should_Return_Room_Booking_Respose_With_Request_Values()
+        public RoomBookingRequestProcessorTest()
         {
-            //Arrange
-            var request = new RoomBookingRequest
+            request = new RoomBookingRequest
             {
                 FullName = "Test Name",
                 Email = "teste@request.com",
                 Date = new DateTime(2021, 10, 20)
             };
 
-            var processor = new RoomBookingRequestProcessor();
+            availableRooms = new List<Room>{ new() { Id = 1 } };
+
+            roomBookingServiceMock = new Mock<IRoomBookingService>();
+            roomBookingServiceMock
+                .Setup(q => q.GetAvailableRooms(request.Date))
+                .Returns(availableRooms);
+
+            processor = new RoomBookingRequestProcessor(roomBookingServiceMock.Object);
+
+        }
+
+        [Fact]
+        public void Should_Return_Room_Booking_Respose_With_Request_Values()
+        {
+            //Arrange
+            //...
 
             //Act
             RoomBookingResult result = processor.BookRoom(request);
@@ -34,5 +56,78 @@ namespace RoomBookingApp.Core
             result.Date.ShouldBe(result.Date);*/
 
         }
+
+        [Fact]
+        public void Should_Throw_Exception_For_Null_Request()
+        {
+            var exception = Should.Throw<ArgumentNullException>(() => processor.BookRoom(null));
+            exception.ParamName.ShouldBe("bookingRequest");
+
+            //or...
+            //Assert.Throws<ArgumentNullException>(() => processor.BookRoom(null));
+
+        }
+
+        [Fact]
+        public void Should_Save_Room_Booking_Request()
+        {
+            RoomBooking savedBooking = null;
+            roomBookingServiceMock
+                .Setup(q => q.Save(It.IsAny<RoomBooking>()))
+                .Callback<RoomBooking>(booking => savedBooking = booking);
+
+            processor.BookRoom(request);
+
+            roomBookingServiceMock
+                .Verify(q => q.Save(It.IsAny<RoomBooking>()), Times.Once);
+
+            savedBooking.ShouldNotBeNull();
+            savedBooking.FullName.ShouldBe(request.FullName);
+            savedBooking.Email.ShouldBe(request.Email);
+            savedBooking.Date.ShouldBe(request.Date);
+            savedBooking.RoomId.ShouldBe(availableRooms.FirstOrDefault().Id);
+
+        }
+
+        [Fact]
+        public void Should_Not_Save_Room_Booking_Request_If_None_Available()
+        {
+            availableRooms.Clear();
+            processor.BookRoom(request);
+
+            roomBookingServiceMock
+                .Verify(q => q.Save(It.IsAny<RoomBooking>()), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(BookingResultFlag.Failure, false)]
+        [InlineData(BookingResultFlag.Sucess, true)]
+        public void Should_Return_SuccessOrFailure_Flag_In_Result(BookingResultFlag bookingResultFlag, bool isAvailable)
+        {
+            if (!isAvailable)
+                availableRooms.Clear();
+
+            var result = processor.BookRoom(request);
+            bookingResultFlag.ShouldBe(result.Flag);
+        }
+
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(null, false)]
+        public void Should_Return_RoombookingId_In_Result(int? roomBookingId, bool isAvailable)
+        {
+            if (!isAvailable)
+                availableRooms.Clear();
+            else
+            {
+                roomBookingServiceMock
+                    .Setup(q => q.Save(It.IsAny<RoomBooking>()))
+                    .Callback<RoomBooking>(booking => booking.Id = roomBookingId.Value);
+            }
+
+            var result = processor.BookRoom(request);
+            result.RoomBookingId.ShouldBe(roomBookingId);
+        }
+
     }
 }
--- RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -1,20 +1,56 @@
-﻿using RoomBookingApp.Core.Models;
+﻿using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Core.Domain;
+using RoomBookingApp.Core.Enums;
+using RoomBookingApp.Core.Models;
 
 namespace RoomBookingApp.Core.Processors
 {
     public class RoomBookingRequestProcessor
     {
-        public RoomBookingRequestProcessor()
+        private readonly IRoomBookingService roomBookingService;
+
+        public RoomBookingRequestProcessor(IRoomBookingService roomBookingService)
         {
+            this.roomBookingService = roomBookingService;
         }
 
-        public RoomBookingResult BookRoom(RoomBookingRequest request)
-            => new RoomBookingResult
+        public RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            if (bookingRequest is null)
+                throw new ArgumentNullException(nameof(bookingRequest));
+
+            var availableRooms = roomBookingService.GetAvailableRooms(bookingRequest.Date);
+            var result = CreateRoomBookingObject<RoomBookingResult>(bookingRequest);
+
+            if (availableRooms.Any())
             {
-                FullName = request.FullName,
-                Date = request.Date,
-                Email = request.Email
+                var room = availableRooms.First();
+                var roomBooking = CreateRoomBookingObject<RoomBooking>(bookingRequest);
+                roomBooking.RoomId = room.Id;
+
+                roomBookingService.Save(roomBooking);
+
+                result.RoomBookingId = roomBooking.Id;
+                result.Flag = BookingResultFlag.Sucess;
+
+            } else
+            {
+                result.Flag = BookingResultFlag.Failure;
+            }
+
+            return result;
+        }
+
+        private TRoomBooking CreateRoomBookingObject<TRoomBooking>(RoomBookingRequest bookingRequest) where TRoomBooking 
+            : RoomBookingBase, new()
+        {
+            return new TRoomBooking
+            {
+                FullName = bookingRequest.FullName,
+                Date = bookingRequest.Date,
+                Email = bookingRequest.Email
             };
+        }
 
     }
 }
